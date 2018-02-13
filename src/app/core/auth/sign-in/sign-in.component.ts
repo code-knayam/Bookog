edfb60ae108038e7f9d50f346180b8811eca2e9f
code-knayam <mayankjain1394@gmail.com>
2018-02-13 20:19:31 +0530

    Completed Sign In Form.

--- src/app/core/auth/sign-in/sign-in.component.ts
+++ src/app/core/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
+import * as CONSTANTS from '../../../shared/constants';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  signInForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.signInForm = new FormGroup({
+      email: new FormControl( null, [Validators.required, Validators.pattern(CONSTANTS.EMAIL_REGEX) ] ),
+      password: new FormControl( null, [Validators.required, Validators.minLength(8), Validators.pattern(CONSTANTS.PASSWORD_REGEX) ])
+    });
   }
 
 }
