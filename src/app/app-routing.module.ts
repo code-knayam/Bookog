92eae39211199c051f1b80f9cfaad857d7d527ad
code-knayam <mayankjain1394@gmail.com>
2018-02-15 11:14:38 +0530

    Added new components for dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LandingComponent } from './core/landing/landing.component';
 import { SignInComponent } from './core/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
 import { CoreComponent } from './core/core.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
     {path: '', component: CoreComponent, children: [
         {path: '', component: LandingComponent},
         {path: 'signIn', component: SignInComponent},
         {path: 'signUp', component: SignUpComponent}
-    ]}
+    ]},
+    {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { SignInComponent } from './core/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreComponent } from './core/core.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { BookEditComponent } from './dashboard/book-edit/book-edit.component';
+import { BookListComponent } from './dashboard/book-list/book-list.component';
+import { BookItemComponent } from './dashboard/book-item/book-item.component';
+import { MenuComponent } from './dashboard/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,13 @@ import { CoreComponent } from './core/core.component';
     HeaderComponent,
     SignInComponent,
     SignUpComponent,
-    CoreComponent
+    CoreComponent,
+    DashboardComponent,
+    HomeComponent,
+    BookEditComponent,
+    BookListComponent,
+    BookItemComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
