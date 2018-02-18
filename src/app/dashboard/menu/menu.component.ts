033d89cb4b64b7abcd89d4c78e5140df2fc11385
code-knayam <mayankjain1394@gmail.com>
2018-02-18 13:02:14 +0530

    Added Dashboard Menu

--- src/app/dashboard/menu/menu.component.ts
+++ src/app/dashboard/menu/menu.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  isOpenNav = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggleNav() {
+    this.isOpenNav = !this.isOpenNav;
+  }
 }
